c9d2c4fd82a479bf75162c6e7a7166cc6af59493
n13z <enonethreezed@users.noreply.github.com>
2020-11-29 01:10:30 +0100

    Update wazuh_bootstrap.sh
    Tabulation O:)

--- Vagrant/wazuh_bootstrap.sh
+++ Vagrant/wazuh_bootstrap.sh
@@ -5,10 +5,10 @@ export DEBIAN_FRONTEND=noninteractive
 install_wazuh() {
 	sudo apt update
 	sudo apt -y dist-upgrade
-    # https://documentation.wazuh.com/4.0/installation-guide/open-distro/all-in-one-deployment/unattended-installation.html
-    curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.0/resources/open-distro/unattended-installation/all-in-one-installation.sh 
-    chmod 700 ~/all-in-one-installation.sh
-    sudo ~/all-in-one-installation.sh
+	# https://documentation.wazuh.com/4.0/installation-guide/open-distro/all-in-one-deployment/unattended-installation.html
+    	curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.0/resources/open-distro/unattended-installation/all-in-one-installation.sh 
+    	chmod 700 ~/all-in-one-installation.sh
+    	sudo ~/all-in-one-installation.sh
 }
 
 	
