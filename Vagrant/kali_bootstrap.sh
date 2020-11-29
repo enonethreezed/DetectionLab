5d73dff61b85198ac87e0bcec428fbc86f041179
enonethreezed <n13z@cenobyte.org>
2020-11-29 01:08:39 +0100

    Fixing syntax and IP address setup errors.

--- Vagrant/kali_bootstrap.sh
+++ Vagrant/kali_bootstrap.sh
@@ -25,19 +25,19 @@ fix_eth1_static_ip() {
   # that eth1 has a static IP set. We workaround this by setting a static DHCP lease.
   echo -e 'interface "eth1" {
     send host-name = gethostname();
-    send dhcp-requested-address 192.168.38.105;
+    send dhcp-requested-address 192.168.38.254;
   }' >>/etc/dhcp/dhclient.conf
   netplan apply
   # Fix eth1 if the IP isn't set correctly
   ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
-  if [ "$ETH1_IP" != "192.168.38.105" ]; then
+  if [ "$ETH1_IP" != "192.168.38.254" ]; then
     echo "Incorrect IP Address settings detected. Attempting to fix."
     ifdown eth1
     ip addr flush dev eth1
     ifup eth1
     ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
     if [ "$ETH1_IP" == "192.168.38.105" ]; then
-      echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.105"
+      echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.254"
     else
       echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
       exit 1
--- Vagrant/wazuh_bootstrap.sh
+++ Vagrant/wazuh_bootstrap.sh
@@ -1,8 +1,63 @@
 #! /usr/bin/env bash
 
-# https://documentation.wazuh.com/4.0/installation-guide/open-distro/all-in-one-deployment/unattended-installation.html
+export DEBIAN_FRONTEND=noninteractive
 
-curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.0/resources/open-distro/unattended-installation/all-in-one-installation.sh 
-chmod 700 ~/all-in-one-installation.sh
-sudo ~/all-in-one-installation.sh
+install_wazuh() {
+	sudo apt update
+	sudo apt -y dist-upgrade
+    # https://documentation.wazuh.com/4.0/installation-guide/open-distro/all-in-one-deployment/unattended-installation.html
+    curl -so ~/all-in-one-installation.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.0/resources/open-distro/unattended-installation/all-in-one-installation.sh 
+    chmod 700 ~/all-in-one-installation.sh
+    sudo ~/all-in-one-installation.sh
+}
 
+	
+
+fix_eth1_static_ip() {
+  USING_KVM=$(sudo lsmod | grep kvm)
+  if [ -n "$USING_KVM" ]; then
+    echo "[*] Using KVM, no need to fix DHCP for eth1 iface"
+    return 0
+  fi
+  if [ -f /sys/class/net/eth2/address ]; then
+    if [ "$(cat /sys/class/net/eth2/address)" == "00:50:56:a3:b1:c4" ]; then
+      echo "[*] Using ESXi, no need to change anything"
+      return 0
+    fi
+  fi
+  # There's a fun issue where dhclient keeps messing with eth1 despite the fact
+  # that eth1 has a static IP set. We workaround this by setting a static DHCP lease.
+  echo -e 'interface "eth1" {
+    send host-name = gethostname();
+    send dhcp-requested-address 192.168.38.25;
+  }' >>/etc/dhcp/dhclient.conf
+  netplan apply
+  # Fix eth1 if the IP isn't set correctly
+  ETH1_IP=$(ip -4 addr show eth1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -1)
+  if [ "$ETH1_IP" != "192.168.38.25" ]; then
+    echo "Incorrect IP Address settings detected. Attempting to fix."
+    ifdown eth1
+    ip addr flush dev eth1
+    ifup eth1
+    ETH1_IP=$(ifconfig eth1 | grep 'inet addr' | cut -d ':' -f 2 | cut -d ' ' -f 1)
+    if [ "$ETH1_IP" == "192.168.38.105" ]; then
+      echo "[$(date +%H:%M:%S)]: The static IP has been fixed and set to 192.168.38.25"
+    else
+      echo "[$(date +%H:%M:%S)]: Failed to fix the broken static IP for eth1. Exiting because this will cause problems with other VMs."
+      exit 1
+    fi
+  fi
+
+  # Make sure we do have a DNS resolution
+  while true; do
+    if [ "$(dig +short @8.8.8.8 github.com)" ]; then break; fi
+    sleep 1
+  done
+}
+main() {
+	fix_statix_ip
+	install_wazuh
+}
+
+main
+exit 0
