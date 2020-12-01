c148482049724d751bb6b80eeb0574a9c9e12134
enonethreezed <n13z@cenobyte.org>
2020-12-01 17:52:06 +0100

    Adding RDP.

--- Vagrant/kali_bootstrap.sh
+++ Vagrant/kali_bootstrap.sh
@@ -12,17 +12,26 @@ reconfigure_locales() {
 
 install_ssh() {
 	sudo apt update
-	sudo apt -y dist-upgrade
-	sudo apt-get install -y ssh
+	sudo apt -y dist-upgrade --force-yes
+	sudo apt-get install --force-yes ssh
 	sudo systemctl start ssh.service
 	sudo reboot
+}
+
+setupRDP(){
+	sudo apt-get install --yes --force-yes kali-desktop-xfce xorg xrdp
+	sudo sed -i 's/port=3389/port=3390/g' /etc/xrdp/xrdp.ini
+	wget https://gitlab.com/kalilinux/build-scripts/kali-wsl-chroot/-/raw/master/xfce4.sh
+	chmod +x xfce4.sh
+	sudo ./xfce4.sh
+	sudo systemctl enable xrdp --now
 }
 
 main() {
 	reconfigure_locales
 	install_ssh
+	setupRDP
 }
 
 main
 exit 0
-
